d10754b657f9f90a6f7a07a8b1118316298d8cb7
AhmadSaadat <saadatahmad267@gmail.com>
2023-09-21 21:24:21 +0330

    Add products,service,interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/domain/product';
+import { ProductService } from 'src/service/product.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'InventorySample';
+export class AppComponent implements OnInit {
+
+  products :  any;
+  
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+      this.productService.getProductsMini().then((data) => {
+          this.products = data;
+      });
+  }
+
+  // getSeverity(status: string) {
+  //     switch (status) {
+  //         case 'INSTOCK':
+  //             return 'success';
+  //         case 'LOWSTOCK':
+  //             return 'warning';
+  //         case 'OUTOFSTOCK':
+  //             return 'danger';
+  //     }
+  // }
+
 }
